a925728804d95f8b711cb8822eff0e84152ccad2
JamStop <jimmyyue1@gmail.com>
2016-02-13 15:06:11 -0800

    minor storyboard changes + mainView

---
+++ ProjectArthur/ProjectArthur/Views/Main Views/ArthurMainView.swift
@@ -0,0 +1,19 @@
+//
+//  ArthurMainView.swift
+//  ProjectArthur
+//
+//  Created by Jimmy Yue on 2/13/16.
+//  Copyright © 2016 TheInitiative. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ArthurMainView: UIView {
+
+    
+    func audio() {
+        AVAudioSession.sharedInstance().outputLatency
+    }
+
+}
