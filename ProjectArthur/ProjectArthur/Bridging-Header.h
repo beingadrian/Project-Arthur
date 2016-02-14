5a8056f1387c8060b418ebeb316c2e7e6ee698a0
JamStop <jimmyyue1@gmail.com>
2016-02-13 17:13:19 -0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	ProjectArthur/ProjectArthur.xcodeproj/project.pbxproj

---
+++ ProjectArthur/ProjectArthur/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  ProjectArthur
+//
+//  Created by Jimmy Yue on 2/13/16.
+//  Copyright © 2016 TheInitiative. All rights reserved.
+//
+
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h>
+#import <EstimoteSDK/EstimoteSDK.h>
---
+++ ProjectArthur/ProjectArthur/Views/Main Views/ArthurMainView.swift
@@ -0,0 +1,20 @@
+//
+//  ArthurMainView.swift
+//  ProjectArthur
+//
+//  Created by Jimmy Yue on 2/13/16.
+//  Copyright © 2016 TheInitiative. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AudioKit
+
+class ArthurMainView: UIView {
+
+    
+    func audio() {
+
+    }
+
+}
